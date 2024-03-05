b406c93877ac14664361ad6fe579879df13ba192
Elaidzha1940 <eli080293@gmail.com>
2024-03-05 19:43:40 +0300

    Added GridSwiftUI

---
+++ SwiftUI2024/Grid/GridSwiftUI.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: SwiftUI2024
+//  File: GridSwiftUI.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 05.03.2024
+//
+//  */
+
+import SwiftUI
+
+struct GridSwiftUI: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    GridSwiftUI()
+}
