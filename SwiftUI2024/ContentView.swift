210d774f71c286d76a911000442d06cade8225ff
Elaidzha1940 <eli080293@gmail.com>
2024-03-05 15:39:45 +0300

    SwiftUI2024

---
+++ SwiftUI2024/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI2024
+//
+//  Created by Elaidzha Shchukin on 05.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI2024/SwiftUI2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2024App.swift
+//  SwiftUI2024
+//
+//  Created by Elaidzha Shchukin on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
